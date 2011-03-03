bd452ee45b4f3d178c1b8794c8ac4aa3b754000d
Patrick Hayes <patrick.d.hayes@gmail.com>
2011-03-03 19:17:58 +0000

    Initial Comit

---
+++ includes/behaviors/js/openlayers_behavior_geojump.js
@@ -0,0 +1,53 @@
+/**
+ * @file
+ * JS Implementation of OpenLayers behavior.
+ */
+
+(function($) {
+  
+/**
+ * geojump Behavior
+ */
+Drupal.behaviors.openlayers_behavior_geojump = {
+  'attach': function(context, settings) {
+    var data = $(context).data('openlayers');
+    
+    if (data && data.map.behaviors['openlayers_behavior_geojump']) {
+      
+      jump_text = '                                                   ' + data.map.behaviors['openlayers_behavior_geojump']['jumptext'];
+      
+      selection_layer = new OpenLayers.Layer.Vector('Selection Layer');      
+      
+      
+      data.openlayers.addLayer(selection_layer);
+      
+      $(data.openlayers.div).before("<div class='geojump-container'><input class='geojump-text' type='text' value='" + jump_text + "'></input></div>");
+
+      $('input.geojump-text').focus(function(event) {
+        $('input.geojump-text').val('').css('color','black');
+      });
+      
+      $('input.geojump-text').keydown(function(event) {
+        // Trigger on enter or tab
+        if (event.keyCode == 13 || event.keyCode == 9) {
+          $.ajax({
+            type: 'GET',
+            url: Drupal.settings.basePath + 'geojump',
+            data: 'query=' + $('input.geojump-text').val(),
+            dataType: 'json',
+            success: function(result){
+              // If it's an area, zoom to that area
+        	    var projection = new OpenLayers.Projection(data.openlayers.baseLayer.projection.getCode());
+        	    var displayProjection = new OpenLayers.Projection(data.openlayers.displayProjection.getCode());
+        	    var bounds = new OpenLayers.Bounds(result.box.west, result.box.south, result.box.east, result.box.north).transform(displayProjection, projection);
+              console.log(bounds);
+                
+              data.openlayers.zoomToExtent(bounds);
+           }
+         });
+        }
+      });
+    }
+  }
+};
+})(jQuery);
