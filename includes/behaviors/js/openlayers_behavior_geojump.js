6cf083fadded7d83f4ca3511a88b09e1fa6eec50
Patrick Hayes <patrick.d.hayes@gmail.com>
2011-03-03 20:03:38 +0000

    Cleanup

--- includes/behaviors/js/openlayers_behavior_geojump.js
+++ includes/behaviors/js/openlayers_behavior_geojump.js
@@ -14,12 +14,7 @@ Drupal.behaviors.openlayers_behavior_geojump = {
     
     if (data && data.map.behaviors['openlayers_behavior_geojump']) {
       
-      jump_text = '                                                   ' + data.map.behaviors['openlayers_behavior_geojump']['jumptext'];
-      
-      selection_layer = new OpenLayers.Layer.Vector('Selection Layer');      
-      
-      
-      data.openlayers.addLayer(selection_layer);
+      jump_text = '                                                ' + data.map.behaviors['openlayers_behavior_geojump']['jumptext'];
       
       $(data.openlayers.div).before("<div class='geojump-container'><input class='geojump-text' type='text' value='" + jump_text + "'></input></div>");
 
@@ -37,10 +32,9 @@ Drupal.behaviors.openlayers_behavior_geojump = {
             dataType: 'json',
             success: function(result){
               // If it's an area, zoom to that area
-        	    var projection = new OpenLayers.Projection(data.openlayers.baseLayer.projection.getCode());
-        	    var displayProjection = new OpenLayers.Projection(data.openlayers.displayProjection.getCode());
-        	    var bounds = new OpenLayers.Bounds(result.box.west, result.box.south, result.box.east, result.box.north).transform(displayProjection, projection);
-              console.log(bounds);
+       	    var projection = new OpenLayers.Projection(data.openlayers.baseLayer.projection.getCode());
+            var displayProjection = new OpenLayers.Projection(data.openlayers.displayProjection.getCode());
+            var bounds = new OpenLayers.Bounds(result.box.west, result.box.south, result.box.east, result.box.north).transform(displayProjection, projection);
                 
               data.openlayers.zoomToExtent(bounds);
            }
